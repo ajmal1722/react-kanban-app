206ddfa54677c06a2c5cb035c4f8a857cf1c5078
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-11 18:07:43 +0530

    add task form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import TaskCard from './components/TaskCard'
+import AddTasks from './components/AddTasks'
 
 function App() {
 
   return (
     <>
+      <AddTasks/>
       <TaskCard/>
     </>
   )
---
+++ src/components/AddTasks.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+const AddTasks = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [dueDate, setDueDate] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const task = {
+            title,
+            description,
+            dueDate
+        }
+        console.log('task', task)
+    };
+
+    return (
+        <div className='flex justify-center pt-8'>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
+                />
+                <input
+                    type="text"
+                    placeholder="Description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
+                />
+                <input
+                    type="date"
+                    placeholder="Due Date"
+                    value={dueDate}
+                    onChange={(e) => setDueDate(e.target.value)}
+                    className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
+                />
+                <button type="submit" className='text-gray-200 font-semibold bg-green-600 px-6 py-2 rounded-lg'>
+                    Add Task
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default AddTasks;
