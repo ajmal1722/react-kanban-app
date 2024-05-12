b3ea3b4b2e40e5afd597fd02474565c069117436
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-12 20:30:18 +0530

    conditional rendering for form

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import toast, { Toaster } from 'react-hot-toast';
+import { CiCirclePlus } from 'react-icons/ci'
 
 const AddTasks = () => {
+    const [showForm, setShowForm] = useState(false);
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [dueDate, setDueDate] = useState('');
     const [taskLists, setTaskLists] = useState([]);
+
+    const displayForm = () => {
+        console.log('button clicked...')
+        setShowForm(prev => !prev)
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -25,7 +32,7 @@ const AddTasks = () => {
             description,
             dueDate
         }
-        
+
         setTaskLists((prev) => {
             const list = [...prev, task];
 
@@ -41,38 +48,44 @@ const AddTasks = () => {
         setDescription('');
         setDueDate('');
     };
-    
+
 
     return (
         <>
-            <Toaster/>
-            <div className='flex justify-center pt-8'>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        placeholder="Title"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
-                    />
-                    <input
-                        type="text"
-                        placeholder="Description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
-                    />
-                    <input
-                        type="date"
-                        placeholder="Due Date"
-                        value={dueDate}
-                        onChange={(e) => setDueDate(e.target.value)}
-                        className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
-                    />
-                    <button type="submit" className='text-gray-200 font-semibold bg-green-600 px-6 py-2 rounded-lg'>
-                        Add Task
-                    </button>
-                </form>
+            <Toaster />
+            <div className='flex justify-center pt-16 my-8'>
+                {showForm ?
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
+                        />
+                        <input
+                            type="text"
+                            placeholder="Description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
+                        />
+                        <input
+                            type="date"
+                            placeholder="Due Date"
+                            value={dueDate}
+                            onChange={(e) => setDueDate(e.target.value)}
+                            className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
+                        />
+                        <button type="submit" className='text-gray-200 font-semibold bg-green-600 px-6 py-2 rounded-lg'>
+                            Add Task
+                        </button>
+                    </form> :
+                    <div onClick={displayForm} className='text-lg text-white bg-green-600 px-6 py-2 rounded-lg cursor-pointer flex gap-2'>
+                        Create a new Task
+                        <CiCirclePlus className='mt-1' />
+                    </div>
+                }
             </div>
         </>
     );
---
+++ src/components/ListTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListTask = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default ListTask
