c8512ad70e1b418d0e8a368e66b493c80893bb62
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-12 19:06:12 +0530

    hot toast added for error and succesfull message

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import toast, { Toaster } from 'react-hot-toast';
 
 const AddTasks = () => {
     const [title, setTitle] = useState('');
@@ -9,6 +10,14 @@ const AddTasks = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if (title.length <= 3) {
+            return toast.error('Title must have 3 letters')
+        }
+
+        if (description.length <= 3) {
+            return toast.error('description must have 3 letters')
+        }
 
         const task = {
             id: uuidv4(),
@@ -25,6 +34,8 @@ const AddTasks = () => {
         });
         console.log('task', taskLists)
 
+        toast.success('Your task is created succesfully!')
+
         // Clear input fields
         setTitle('');
         setDescription('');
@@ -33,34 +44,37 @@ const AddTasks = () => {
     
 
     return (
-        <div className='flex justify-center pt-8'>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
-                />
-                <input
-                    type="text"
-                    placeholder="Description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
-                />
-                <input
-                    type="date"
-                    placeholder="Due Date"
-                    value={dueDate}
-                    onChange={(e) => setDueDate(e.target.value)}
-                    className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
-                />
-                <button type="submit" className='text-gray-200 font-semibold bg-green-600 px-6 py-2 rounded-lg'>
-                    Add Task
-                </button>
-            </form>
-        </div>
+        <>
+            <Toaster/>
+            <div className='flex justify-center pt-8'>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
+                    />
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
+                    />
+                    <input
+                        type="date"
+                        placeholder="Due Date"
+                        value={dueDate}
+                        onChange={(e) => setDueDate(e.target.value)}
+                        className='border border-gray-300 rounded-lg px-4 py-2 mb-2 mr-2'
+                    />
+                    <button type="submit" className='text-gray-200 font-semibold bg-green-600 px-6 py-2 rounded-lg'>
+                        Add Task
+                    </button>
+                </form>
+            </div>
+        </>
     );
 };
 
