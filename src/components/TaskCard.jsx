85b5c7c012fcc1dc87dd755702a5dd421d3e4775
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-13 12:50:59 +0530

    added style

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -6,18 +6,21 @@ const TaskCard = ({ task }) => {
     const date = task.dueDate
 
     return (
-        <div className='text-2xl border rounded-lg p-2 m-2 bg-gray-50 cursor-pointer'>
-            <div className="flex gap-3 justify-end text-lg px-3 mb-3">
-                <MdEdit/>
-                <MdDelete/>
-            </div>
-            <div className="flex justify-between">
-                <h1 className='font-semibold text-gray-700'>
-                    { title }
-                </h1>
-                <h1 className='text-lg font-semibold text-gray-700 mx-2'>
+        <div className='text-2xl border rounded-lg p-4 m-2 bg-gray-50 cursor-pointer '>
+            <div className="flex justify-between mb-3">
+                <h1 className='text-lg font-semibold text-gray-700'>
                     { date }
                 </h1>
+                <div className="flex gap-3 justify-end text-lg px-3">
+                    <MdEdit className='hover:text-green-700'/>
+                    <MdDelete className='hover:text-red-700'/>
+                </div>
+            </div>
+            <div className="flex justify-between">
+                <h1 className='font-semibold text-violet-800'>
+                    { title }
+                </h1>
+                
             </div>
             <p className='text-base my-2'>
                 { description }
