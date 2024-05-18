002d676e84907d30d83d2e33870842cf0c5bc67e
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-18 17:57:33 +0530

    Update in ListTask component

--- src/components/ListTask.jsx
+++ src/components/ListTask.jsx
@@ -1,7 +1,6 @@
 import TaskCard from './TaskCard'
 import { useSelector } from 'react-redux'
 import { Droppable } from 'react-beautiful-dnd'
-import { v4 as uuidv4 } from 'uuid';
 
 const ListTask = () => {
     const statuses = ['Pending', 'Progress', 'Completed']
@@ -9,28 +8,30 @@ const ListTask = () => {
     console.log('muhammed ajmal', tasks)
 
     return (
-        <div className='flex lg:gap-16 md:gap-8 gap-6 justify-center flex-wrap py-8  px-6'>
-            {statuses.map((item, index) => {
-                return (
-                    <Droppable key={item} droppableId={item}>
-                        {(provided) => (
-                            <div ref={provided.innerRef} {...provided.droppableProps}
-                                className='border lg:max-w-96 sm:max-w-80 w-full rounded-lg min-h-96'>
-                                <h1 className='text-xl font-semibold text-white bg-gray-600 text-center p-4 rounded-t-lg mb-6'>
-                                    {item}
-                                </h1>
-                                {tasks &&
-                                    tasks.filter(task => task.status === item)
-                                        .map(task => (
-                                            <TaskCard key={task.id} task={task} index={index}/>
-                                        ))
-                                }
-                                {provided.placeholder}
-                            </div>
-                        )}
-                    </Droppable>
-                )
-            })}
+        <div className='flex lg:gap-16 md:gap-8 gap-6 justify-center flex-wrap py-8 px-6'>
+            {statuses.map((status) => (
+                <Droppable key={status} droppableId={status}>
+                    {(provided) => ( 
+                        <div 
+                            ref={provided.innerRef} 
+                            {...provided.droppableProps}
+                            className='border lg:max-w-96 sm:max-w-80 w-full rounded-lg min-h-96'
+                        >
+                            <h1 className='text-xl font-semibold text-white bg-gray-600 text-center p-4 rounded-t-lg mb-6'>
+                                {status}
+                            </h1>
+                            {tasks && 
+                                tasks
+                                    .filter(task => task.status === status)
+                                    .map((task, index) => (
+                                        <TaskCard key={task.id} task={task} index={index} /> 
+                                    ))
+                            }
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            ))}
         </div>
     )
 }
