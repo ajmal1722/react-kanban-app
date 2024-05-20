3fd468e11717ac754fe21d1531dbf5f4990387b3
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-20 09:28:19 +0530

    removed unwanted strict mode and unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import './App.css'
 import AddTasks from './components/AddTasks'
-import { useState, useEffect } from 'react'
 import ListTask from './components/ListTask'
 import { DragDropContext } from 'react-beautiful-dnd'
 
 function App() {
-  const [tasks, setTasks] = useState([])
-  // const [taskLists, setTaskLists] = useState([]);
-
-  console.log('taskFrom app:', tasks)
-
-  useEffect(() => {
-    setTasks(JSON.parse(localStorage.getItem('tasks')))
-  }, [])
+  
   return (
     <DragDropContext>
       <AddTasks/>
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import store from './redux/store.js'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
 )
