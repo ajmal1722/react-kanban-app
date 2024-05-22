f7f13871d596b0adc4406ce94838f553d04c2f62
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-22 10:26:14 +0530

    date added on modal

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -31,10 +31,13 @@ const EditModal = ({EditModal, setShowEditModal}) => {
                                 <label for="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
                                 <input type="text" name="description" id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white mb-4" required />
                             </div>
+                            <div>
+                                <label for="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                                <input type="date" name="date" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white mb-4" required />
+                            </div>
                             <button type="submit" className="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                                 Update
                             </button>
-                            
                         </form>
                     </div>
                 </div>
