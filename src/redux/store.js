23397de78c356a65fc33a99bad91253cf7edfdb3
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-27 20:57:08 +0530

    used redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import taskReducer from "./tasks/taskSlice";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage' 
 
-export default configureStore({
-    reducer: {
-        tasks: taskReducer
-    }
-})
+const rootReducer = combineReducers({
+    tasks: taskReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+    const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+    const store = configureStore({
+    reducer: persistedReducer,
+});
+
+
+const persistor = persistStore(store);
+
+export { store, persistor };
